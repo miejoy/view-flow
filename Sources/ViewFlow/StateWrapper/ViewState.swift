d55612b4f10eaa3db55df6cfbd6699d471c7155e
Raymond-HuangL <raymond0huang@gmail.com>
2024-05-11 22:32:46 +0800

    ViewState 添加初始化方法

--- Sources/ViewFlow/StateWrapper/ViewState.swift
+++ Sources/ViewFlow/StateWrapper/ViewState.swift
@@ -24,6 +24,10 @@ public struct ViewState<State: StorableViewState> : DynamicProperty {
     @Environment(\.recordViewState)
     var recordViewState
     
+    public init(initialValue value: State) {
+        self._storage = .init(wrappedValue: .init(state: value))
+    }
+    
     public init(wrappedValue value: State) {
         self._storage = .init(wrappedValue: .init(state: value))
     }
